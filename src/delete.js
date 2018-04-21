8ab2dc273bd9d07e3b9c52ebde983bc05735c1ec
Kseniaveh <31632695+Kseniaveh@users.noreply.github.com>
2018-04-21 17:49:19 +0300

    Update delete.js

--- src/delete.js
+++ src/delete.js
@@ -1,6 +1,6 @@
 const User = require('./bd');
 (async () => {
-	let doomedUser = {login: 'KseniaVeh'};
+	let doomedUser = {login: 'Vehova Ksenia'};
 	doomedUser = new User(newUser);
 	await User.deleteOne(doomedUser);
 	process.exit(0);
